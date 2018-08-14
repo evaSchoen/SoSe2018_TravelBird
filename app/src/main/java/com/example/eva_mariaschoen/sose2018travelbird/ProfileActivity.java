ab443392b19d31b4537e7e70e0b4b7d04e02610b
evaSchoen <eva-mariaschoen@Eva-Marias-MBP.fritz.box>
2018-08-14 16:43:35 +0200

    Register/LogIn, Drawer Menu in Profile

--- app/src/main/java/com/example/eva_mariaschoen/sose2018travelbird/ProfileActivity.java
+++ app/src/main/java/com/example/eva_mariaschoen/sose2018travelbird/ProfileActivity.java
@@ -1,13 +1,34 @@
 package com.example.eva_mariaschoen.sose2018travelbird;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerlayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
+        mDrawerlayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
