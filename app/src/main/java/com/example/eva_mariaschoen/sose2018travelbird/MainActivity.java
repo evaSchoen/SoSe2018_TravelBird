3616f2b0d01a3f15a98d11e016e1d44eece49ebb
evaSchoen <eva-mariaschoen@Eva-Marias-MBP.fritz.box>
2018-08-14 14:17:01 +0200

    first commit

---
+++ app/src/main/java/com/example/eva_mariaschoen/sose2018travelbird/MainActivity.java
@@ -0,0 +1,235 @@
+package com.example.eva_mariaschoen.sose2018travelbird;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new MainActivity.SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
+
+        private Button buttonRegister;
+        private EditText editTextEmail;
+        private EditText editTextPassword;
+        private ProgressDialog progressDialog;
+
+        private Integer currTab;
+
+        private FirebaseAuth firebaseAuth;
+
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            currTab = getArguments().getInt(ARG_SECTION_NUMBER);
+
+
+            firebaseAuth = FirebaseAuth.getInstance();
+
+            progressDialog = new ProgressDialog(getActivity());
+
+            buttonRegister = (Button) rootView.findViewById(R.id.buttonRegister);
+            editTextEmail = (EditText) rootView.findViewById(R.id.editTextEmail);
+            editTextPassword = (EditText) rootView.findViewById(R.id.editTextPassword);
+
+            if(currTab == 1) {
+                buttonRegister.setText("Register");
+            } else {
+                buttonRegister.setText("Login");
+            }
+
+            buttonRegister.setOnClickListener(this);
+            return rootView;
+        }
+
+
+        public void onClick(View view) {
+
+            if (view == buttonRegister) {
+                if(currTab == 1){
+                    registerUser();}
+                else {
+                    loginUser();
+                }
+
+            }
+        }
+
+        private void loginUser() {
+
+            String email = editTextEmail.getText().toString().trim();
+            String password = editTextPassword.getText().toString().trim();
+
+            progressDialog.setMessage("LogIn User");
+            progressDialog.show();
+
+            firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressDialog.dismiss();
+                            if(task.isSuccessful()){
+                                Toast.makeText(getActivity(), "Loged in successfully!", Toast.LENGTH_SHORT).show();
+                                Intent profileIntent = new Intent(getActivity(), ProfileActivity.class);
+                                startActivity(profileIntent);
+                            }
+                            else {
+                                Toast.makeText(getActivity(), "Could not log in User!", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
+        }
+
+        private void registerUser() {
+
+            String email = editTextEmail.getText().toString().trim();
+            String password = editTextPassword.getText().toString().trim();
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(getActivity(), "enter your email here", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(getActivity(), "enter password here", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            progressDialog.setMessage("Registering User");
+            progressDialog.show();
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            progressDialog.dismiss();
+                            if (task.isSuccessful()) {
+
+                                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                                Toast.makeText(getActivity(), "Registered successfully!", Toast.LENGTH_SHORT).show();
+
+                                Intent profileIntent = new Intent(getActivity(), ProfileActivity.class);
+                                startActivity(profileIntent);
+                            } else {
+                                Toast.makeText(getActivity(), "Could not register user!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public static class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return MainActivity.PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+    }
+
+}
