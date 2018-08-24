936ec671c92fac129d808bd99127171874f7c796
RaphaelaScherer <raphaelascherer@gmx.de>
2018-08-24 12:05:34 +0200

    Anlegen der Activity layouts und den Activities

---
+++ app/src/main/java/com/example/eva_mariaschoen/sose2018travelbird/NewEntryActivity.java
@@ -0,0 +1,13 @@
+package com.example.eva_mariaschoen.sose2018travelbird;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewEntryActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_entry);
+    }
+}
